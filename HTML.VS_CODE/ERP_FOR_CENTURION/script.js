40681bb72dfbd91a419f667694cf42effeeef67a
appanavijay <134784286+appanavijay@users.noreply.github.com>
2023-05-27 18:02:42 +0530

    Add files via upload

---
+++ HTML.VS_CODE/ERP_FOR_CENTURION/script.js
@@ -0,0 +1,5 @@
+let barsEl = document.querySelector('.bars');
+let iconBarsEl = document.querySelector('.icon-bars');
+barsEl.addEventListener('click', () => {
+   iconBarsEl.classList.toggle('show');
+});
